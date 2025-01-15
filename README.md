8437c86c2a00fb53add55821ff61abcb1594dfb2
Jarkko Tuikka <jarkko.tuikka@gmail.com>
2025-01-15 16:47:08 +0200

    3.1. - 3.11

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fullstackopen-part3
+
+https://fullstackopen-phonebook.azurewebsites.net/
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -12,6 +13,9 @@ morgan.token('body', function getBody (req) {
     }
 })
 
+
+app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 //app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -110,7 +114,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
